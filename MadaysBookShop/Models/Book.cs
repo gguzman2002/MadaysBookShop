9c4f90cc8fdb8621453c15a2937cb67bbc15cb38
gabrielmaday <40696114+gabrielmaday@users.noreply.github.com>
2022-11-04 09:44:58 -0400

    Add project files.

---
+++ MadaysBookShop/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿namespace MadaysBookShop.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? ShortDescription { get; set; }
+        public string? LongDescription { get; set; }
+        public decimal Price { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? ImageThumbnailUrl { get; set; }
+        public bool IsBookOfTheWeek { get; set; }
+        public bool InStock { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; } = default!;
+    }
+}
