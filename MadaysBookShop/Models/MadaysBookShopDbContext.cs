f195e1219554d193a9aafa46a8fcc98ca573d4b4
gabrielmaday <40696114+gabrielmaday@users.noreply.github.com>
2022-11-11 18:01:10 -0500

    EFCore was just added through nuget packages, DBContext class initially created

---
+++ MadaysBookShop/Models/MadaysBookShopDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MadaysBookShop.Models
+{
+    public class MadaysBookShopDbContext: DbContext
+    {
+        public MadaysBookShopDbContext(DbContextOptions<MadaysBookShopDbContext>
+            options) : base(options)
+        {
+        }
+    }
+}
