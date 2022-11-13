1f16bac612602e1b5c7873f1eafbfc4f1d238048
gabrielmaday <40696114+gabrielmaday@users.noreply.github.com>
2022-11-13 14:01:35 -0500

    CategoryRepository created, fully implementing ICategoryRepository

---
+++ MadaysBookShop/Models/CategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace MadaysBookShop.Models
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly MadaysBookShopDbContext _madaysBookShopDbContext;
+
+        public CategoryRepository(MadaysBookShopDbContext madaysBookShopDbContext)
+        {
+            _madaysBookShopDbContext = madaysBookShopDbContext;
+        }
+
+        public IEnumerable<Category> AllCategories =>
+            _madaysBookShopDbContext.Categories.OrderBy(c => c.CategoryName);
+    }
+}
