b261f7a7fec8b10a9aaf5eb75265f8982b91c30f
gabrielmaday <40696114+gabrielmaday@users.noreply.github.com>
2022-11-18 11:05:50 -0500

    ShoppingCart controller and view model created

---
+++ MadaysBookShop/Controllers/ShoppingCartController.cs
@@ -0,0 +1,17 @@
+﻿using MadaysBookShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MadaysBookShop.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        private readonly IBookRepository _bookRepository;
+        private readonly IShoppingCart _shoppingCart;
+
+        public ShoppingCartController(IBookRepository bookRepository, IShoppingCart shoppingCart)
+        {
+            _bookRepository = bookRepository;
+            _shoppingCart = shoppingCart;
+        }
+    }
+}
---
+++ MadaysBookShop/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,16 @@
+﻿using MadaysBookShop.Models;
+
+namespace MadaysBookShop.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public ShoppingCartViewModel(IShoppingCart shoppingCart, decimal shoppingCartTotal)
+        {
+            ShoppingCart = shoppingCart;
+            shoppingCartTotal = shoppingCartTotal;
+        }
+
+        public IShoppingCart ShoppingCart { get; }
+        public decimal ShoppingCartTotal { get; }
+    }
+}
