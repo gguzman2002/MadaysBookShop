15ed00cb414e7a3518a6aa170cbda43cad8da2ef
gabrielmaday <40696114+gabrielmaday@users.noreply.github.com>
2022-11-18 08:19:58 -0500

    _BookCard partial created and implemented. ShoppingCartItem created.

---
+++ MadaysBookShop/Models/ShoppingCartItem.cs
@@ -0,0 +1,11 @@
+﻿namespace MadaysBookShop.Models
+{
+    public class ShoppingCartItem
+    {
+        public int ShoppingCartItemId { get; set; }
+        public Book Book { get; set; } = default!;
+        public int Amount { get; set; }
+        public string? ShoppingCartId { get; set; }
+
+    }
+}
