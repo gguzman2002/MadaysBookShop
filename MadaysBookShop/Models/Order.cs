4efced6a9bb444758e7b3c0a0d986e54cadc35fa
gabrielmaday <40696114+gabrielmaday@users.noreply.github.com>
2022-11-27 18:17:33 -0500

    Order model, validation logic was created for all the properties.

--- MadaysBookShop/Models/Order.cs
+++ MadaysBookShop/Models/Order.cs
@@ -1,20 +1,64 @@
-﻿namespace MadaysBookShop.Models
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+
+namespace MadaysBookShop.Models
 {
     public class Order
     {
+        [BindNever]
         public int OrderId { get; set; }
         public List<OrderDetail>? OrderDetails { get; set; }
+
+        [Required(ErrorMessage = "Please enter your first name")]
+        [Display(Name = "First name")]
+        [StringLength(50)]
         public string FirstName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter your last name")]
+        [Display(Name = "Last name")]
+        [StringLength(50)]
         public string LastName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter your address")]
+        [Display(Name = "Address Line 1")]
+        [StringLength(100)]
         public string AddressLine1 { get; set; } = string.Empty;
+
+        [Display(Name = "Address Line 2")]
         public string? AddressLine2 { get; set; }
+
+        [Required(ErrorMessage = "Please enter your zip code")]
+        [Display(Name = "Zip code")]
+        [StringLength(10, MinimumLength = 5)]
         public string ZipCode { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter your city")]
+        [StringLength(50)]
         public string City { get; set; } = string.Empty;
+
+        [StringLength(10)]
         public string? State { get; set; }
+
+        [Required(ErrorMessage = "Please enter your country")]
+        [StringLength(50)]
         public string Country { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter your phone number")]
+        [StringLength(25)]
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Phone number")]
         public string PhoneNumber { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(50)]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Invalid email address, please try again")]
         public string Email { get; set; } = string.Empty;
+
+        [BindNever]
         public decimal OrderTotal { get; set; }
+
+        [BindNever]
         public DateTime OrderPlaced { get; set; }
     }
 }
