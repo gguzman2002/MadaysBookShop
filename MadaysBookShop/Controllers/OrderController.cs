780339a4ce619d02651ee91d9ac6d14d26b39474
gabrielmaday <40696114+gabrielmaday@users.noreply.github.com>
2022-11-26 23:00:34 -0500

    OrderController, Checkout view created.

---
+++ MadaysBookShop/Controllers/OrderController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MadaysBookShop.Models;
+
+namespace MadaysBookShop.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IShoppingCart _shoppingCart;
+
+        public OrderController(IOrderRepository orderRepository, IShoppingCart shoppingCart)
+        {
+            _orderRepository = orderRepository;
+            _shoppingCart = shoppingCart;
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+    }
+}
